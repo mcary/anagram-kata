fb6d8216157f2e058d2855dc334550ee7d61a5f4
Marcel M. Cary <marcel@oreilly.com>
2015-01-23 15:19:17 -0800

    Ensure we find no anagrams among a single word

--- spec/anagram_finder_spec.rb
+++ spec/anagram_finder_spec.rb
@@ -17,6 +17,11 @@ describe "AnagramFinder" do
     expect(count).to eq 0
   end
 
+  it "finds no anagrams among one word" do
+    count = finder_for("foo").anagrams.count
+    expect(count).to eq 0
+  end
+
   def finder_for(str)
     AnagramFinder.new(StringIO.new(str))
   end
