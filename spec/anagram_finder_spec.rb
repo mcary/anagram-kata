7a4cee1a7d95df6abc3720291c3ec3cf27c9bae1
Marcel M. Cary <marcel@oreilly.com>
2015-01-23 15:50:25 -0800

    Refactor: Extract permutations helper and join chars sooner

--- spec/anagram_finder_spec.rb
+++ spec/anagram_finder_spec.rb
@@ -7,16 +7,12 @@ class AnagramFinder
     words = io.readlines.map(&:chomp)
     word = words.first
     if word
-      # The to_a.uniq is because duplicate letters result in duplicate
-      # entries: "foo"'s permutation include "foo" and "foo" again, but
-      # with the "o"'s reversed in the 2nd instance.
-      anas = word.chars.permutation.to_a.uniq.select do |chars|
-        w = chars.join
+      anas = permutations(word).select do |w|
         w != word && words.include?(w)
       end
 
       if anas.any?
-        [[word]+anas.map(&:join)]
+        [[word]+anas]
       else
         []
       end
@@ -28,6 +24,13 @@ class AnagramFinder
   private
 
   attr_accessor :io
+
+  def permutations(word)
+    # The to_a.uniq is because duplicate letters result in duplicate
+    # entries: "foo"'s permutation include "foo" and "foo" again, but
+    # with the "o"'s reversed in the 2nd instance.
+    word.chars.permutation.to_a.uniq.map(&:join)
+  end
 end
 
 describe "AnagramFinder" do
