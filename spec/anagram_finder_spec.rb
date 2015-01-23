f5881d096311411695859873f0f0e295ef433fd4
Marcel M. Cary <marcel@oreilly.com>
2015-01-23 15:24:35 -0800

    Regression: Add support for one anagram, breaking the one-word case

--- spec/anagram_finder_spec.rb
+++ spec/anagram_finder_spec.rb
@@ -1,10 +1,16 @@
 class AnagramFinder
   def initialize(io)
+    self.io = io
   end
 
   def anagrams
-    []
+    words = io.readlines.map(&:chomp)
+    [words]
   end
+
+  private
+
+  attr_accessor :io
 end
 
 describe "AnagramFinder" do
@@ -20,6 +26,11 @@ describe "AnagramFinder" do
   it "finds no anagrams among one word" do
     count = finder_for("foo").anagrams.count
     expect(count).to eq 0
+  end
+
+  it "finds one anagram among: dad, add" do
+    count = finder_for("dad\nadd").anagrams.count
+    expect(count).to eq 1
   end
 
   def finder_for(str)
