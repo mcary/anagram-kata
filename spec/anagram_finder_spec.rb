0efa4c3d45280f0a143a8653015baeef05798327
Marcel M. Cary <marcel@oreilly.com>
2015-01-23 15:12:12 -0800

    Add an IO to the AnagramFinder constructor

--- spec/anagram_finder_spec.rb
+++ spec/anagram_finder_spec.rb
@@ -1,8 +1,14 @@
 class AnagramFinder
+  def initialize(io)
+  end
 end
 
 describe "AnagramFinder" do
-  it "intializes" do
-    AnagramFinder.new
+  it "intializes with io" do
+    finder_for("")
+  end
+
+  def finder_for(str)
+    AnagramFinder.new(StringIO.new(str))
   end
 end
