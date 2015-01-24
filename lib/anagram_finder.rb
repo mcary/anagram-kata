fc510b4c28e51d133e7f2428c4c367560809770f
Marcel M. Cary <marcel@oreilly.com>
2015-01-23 16:25:47 -0800

    Use a different algorithm to find anagrams faster
    The current naive algorithm for finding anagrams takes forever (>5min).
    
    Switch to a hash-based algorithm that finds anagrams by bucketting
    words according to a normalized form.  This only works because we
    are asked to find all anagrams in the entire file.
    
    Performance is much better now:
    
        $ irb
        2.1.1 :001 > require './lib/anagram_finder'
         => true
        2.1.1 :002 > require 'benchmark'
         => true
        2.1.1 :003 > af = AnagramFinder.new(File.open("wordlist.txt"))
         => #<AnagramFinder:0x007ffaaedbbd60 @io=#<File:wordlist.txt>>
        2.1.1 :004 > Benchmark.realtime { puts af.anagrams.count }
        20683
         => 1.712718
    
    And the code is actually shorter and simpler, too:
     1 file changed, 4 insertions(+), 20 deletions(-)

--- lib/anagram_finder.rb
+++ lib/anagram_finder.rb
@@ -5,31 +5,15 @@ class AnagramFinder
 
   def anagrams
     words = io.readlines.map(&:chomp)
-    result = []
+    groups = Hash.new { [] }
     words.each do |word|
-      anas = permutations(word).select do |w|
-        w != word && words.include?(w)
-      end
-
-      if anas.any?
-        anas.push word
-        anas = anas.sort
-        unless result.include? anas
-          result.push anas
-        end
-      end
+      normalized = word.chars.sort.join
+      groups[normalized] += [word]
     end
-    result
+    groups.values.select {|g| g.size > 1 }
   end
 
   private
 
   attr_accessor :io
-
-  def permutations(word)
-    # The to_a.uniq is because duplicate letters result in duplicate
-    # entries: "foo"'s permutation include "foo" and "foo" again, but
-    # with the "o"'s reversed in the 2nd instance.
-    word.chars.permutation.to_a.uniq.map(&:join)
-  end
 end
