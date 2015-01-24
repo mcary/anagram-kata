adb1a8ec416f868dacd96627855c155032e440c5
Marcel M. Cary <marcel@oreilly.com>
2015-01-23 16:04:28 -0800

    Extract AnagramFinder to lib/ so we can run it on wordlist.txt

--- spec/anagram_finder_spec.rb
+++ spec/anagram_finder_spec.rb
@@ -1,38 +1,4 @@
-class AnagramFinder
-  def initialize(io)
-    self.io = io
-  end
-
-  def anagrams
-    words = io.readlines.map(&:chomp)
-    result = []
-    words.each do |word|
-      anas = permutations(word).select do |w|
-        w != word && words.include?(w)
-      end
-
-      if anas.any?
-        anas.push word
-        anas = anas.sort
-        unless result.include? anas
-          result.push anas
-        end
-      end
-    end
-    result
-  end
-
-  private
-
-  attr_accessor :io
-
-  def permutations(word)
-    # The to_a.uniq is because duplicate letters result in duplicate
-    # entries: "foo"'s permutation include "foo" and "foo" again, but
-    # with the "o"'s reversed in the 2nd instance.
-    word.chars.permutation.to_a.uniq.map(&:join)
-  end
-end
+require 'anagram_finder'
 
 describe "AnagramFinder" do
   it "intializes with io" do
